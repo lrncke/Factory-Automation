a7924bdb329c00d99d58399ef6520025648cbb01
dar-ius <marmeladenbrotmitbutter@gmail.com>
2025-07-01 15:05:32 +0200

    Stop tracking release and project files

--- Alternative Gateway/.release/control_io.d
+++
@@ -1,7 +0,0 @@
-.release/control_io.o: control_io.c control.h asi5.h control_io.h
-
-control.h:
-
-asi5.h:
-
-control_io.h:
--- Alternative Gateway/.release/startup.d
+++
@@ -1,5 +0,0 @@
-.release/startup.o: startup.c control.h asi5.h
-
-control.h:
-
-asi5.h:
