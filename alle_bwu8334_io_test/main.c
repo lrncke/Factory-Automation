0a4716907c45d08f19f6067a11d4d9a5bd99d7e7
dar-ius <marmeladenbrotmitbutter@gmail.com>
2025-07-07 12:33:30 +0200

    test programm für erste Station hinzugefügt

--- alle_bwu8334_io_test/main.c
+++ alle_bwu8334_io_test/main.c
@@ -27,6 +27,13 @@
  *  local definitions                                                *
  *-------------------------------------------------------------------*/
 
+#define BANDENDE_LINKS 	0x01
+#define BANDENDE_RECHTS 0x01<<4
+#define START_BUTTON 0x01<<7
+#define BAND_RECHTSLAUF 0x01<<1
+#define LAMPE_ROT 0x01<<3
+#define LAMPE_GRUEN 0x01<<7
+
 /*-------------------------------------------------------------------*
  *  external declarations                                            *
  *-------------------------------------------------------------------*/
@@ -62,18 +69,30 @@ int main ( void )
         //cctrl_func.CCtrlBreakpoint();
 
         unsigned char                ctrl_flags;
-        unsigned char                asi5_accodi[2];//Länge entspricht der Anzahl der in der Anlage vorhandenen logischen Adressen
-        unsigned char                asi5_odi[2];//Ausgänge die gesetzt werden sollen (Hex-Werte)
+        unsigned char                asi5_accodi[10];//Länge entspricht der Anzahl der in der Anlage vorhandenen logischen Adressen
+        unsigned char                asi5_odi[10];//Ausgänge die gesetzt werden sollen (Hex-Werte)
 
-        unsigned char               asi5_idi[2];//Eingänge die gelesen werden sollen (Hex-Werte)
+        unsigned char               asi5_idi[10];//Eingänge die gelesen werden sollen (Hex-Werte)
 
         unsigned short 				length 			= 2;
-
+        int zustand = 1;
         /* Access ASi5 logical Slave 1 (1 Byte / Slave) */
         asi5_accodi[0] = 0xFF;
         asi5_accodi[1] = 0xFF;
+        asi5_accodi[2] = 0xFF;
+		asi5_accodi[3] = 0xFF;
+		asi5_accodi[4] = 0xFF;
+		asi5_accodi[5] = 0xFF;
+		asi5_accodi[6] = 0xFF;
+		asi5_accodi[7] = 0xFF;
+		asi5_accodi[8] = 0xFF;
+		asi5_accodi[9] = 0xFF;
 
         cctrl_func.Asi5WriteCtrlAccODI(0, asi5_accodi, 2, 1);
+        cctrl_func.Asi5WriteCtrlAccODI(0, &asi5_accodi[2], 2, 3);
+        cctrl_func.Asi5WriteCtrlAccODI(0, &asi5_accodi[4], 2, 5);
+        cctrl_func.Asi5WriteCtrlAccODI(0, &asi5_accodi[6], 2, 7);
+        cctrl_func.Asi5WriteCtrlAccODI(0, &asi5_accodi[8], 2, 9);
 
         /* init timer function with 10ms ticks */
         cctrl_func.CCtrlInitTimer ( 1, timer_function );
@@ -83,7 +102,21 @@ int main ( void )
 
     	asi5_odi[0] = 0x00; 									//alle ausgänge 0 setzen
     	asi5_odi[1] = 0x00;
+    	asi5_odi[2] = 0x00;
+    	asi5_odi[3] = 0x00;
+    	asi5_odi[4] = 0x00;
+    	asi5_odi[5] = 0x00;
+    	asi5_odi[6] = 0x00;
+    	asi5_odi[7] = 0x00;
+    	asi5_odi[8] = 0x00;
+    	asi5_odi[9] = 0x00;
 		cctrl_func.Asi5WriteASi5Odi(0, asi5_odi, 2, 1);			// odi schreiben
+		cctrl_func.Asi5WriteASi5Odi(0, &asi5_odi[2], 2, 3);			// odi schreiben
+		cctrl_func.Asi5WriteASi5Odi(0, &asi5_odi[4], 2, 5);
+		cctrl_func.Asi5WriteASi5Odi(0, &asi5_odi[6], 2, 7);
+		cctrl_func.Asi5WriteASi5Odi(0, &asi5_odi[8], 2, 9);
+
+		//int flag =
 
 
         for(;;)
@@ -99,21 +132,27 @@ int main ( void )
                 if ( ((unsigned short)(system_ticks - end_timer)) > 50)
                 {
                         end_timer = system_ticks;
+
                         cctrl_func.Asi5ReadASi5Idi(0, asi5_idi, &length, 1);			// es werden 2 bytes von bwu8334 gelesen
 
-                        	if ( (  ((asi5_idi[1]<<8)|asi5_idi[0]) & (0x01 << 7))) {	/* wenn an port 12 HIGH anliegt, dann,...
-                        																	(Hinweis: funktioniert nur, wenn der Port bevor das
-                        																	Programm auf controller geladen wird, beschaltet ist,
-                        																	also die LED orange leuchtet.) */
-                        		asi5_odi[0] = 0x00;
-                        		asi5_odi[1] = 0x01;										// port 9 schalten
-                        	    cctrl_func.Asi5WriteASi5Odi(0, asi5_odi, 2, 1);			// odi schreiben
-                        	}
-							else {
-								asi5_odi[0] = 0x00;
-								asi5_odi[1] = 0x00;
-								cctrl_func.Asi5WriteASi5Odi(0, asi5_odi, 2, 1);
-							}
+
+                        //Zustandsübergänge
+                        if(zustand == 1 && ((asi5_idi[0]) & (START_BUTTON)) && ((asi5_idi[0]) & (BANDENDE_LINKS))){
+                        	zustand = 2;
+                        }
+                        if(zustand == 2 && ((asi5_idi[0]) & ((BANDENDE_RECHTS)))){
+                        	zustand = 1;
+                        }
+
+                        //Ausgaben
+                        if(zustand == 1){
+							asi5_odi[1] = LAMPE_GRUEN;
+							cctrl_func.Asi5WriteASi5Odi(0, &asi5_odi[0], 2, 1);			// odi schreiben
+                        }
+                        if(zustand == 2){
+                    		asi5_odi[1] = BAND_RECHTSLAUF | LAMPE_ROT;
+                    		cctrl_func.Asi5WriteASi5Odi(0, &asi5_odi[0], 2, 1);			// odi schreiben
+                        }
 
                 }
 
